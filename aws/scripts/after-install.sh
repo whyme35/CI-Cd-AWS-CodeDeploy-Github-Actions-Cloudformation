0bb2d8feb5ddef26e19861f734a508dec5ce7b84
Rolands Jankovskis <Rolands.jankovskis@gmail.com>
2022-07-16 00:42:27 +0300

    Update bucket name
    • Update bucket name

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codestackdeploy-webappdeploymentbucket-48tz2q3oyk24/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-1liud17nmvbtl/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
